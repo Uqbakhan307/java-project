7fb04c1b4845a5af7a8ae512c3fc687ffebcaff5
root <root@localhost>
2024-12-17 20:53:46 +0500

    auto publish 1734450825568

---
+++ app.js
@@ -0,0 +1,18 @@
+// Javascript Chapter 1 Test
+
+// Answer of Question 1
+alert(" Welcome to my Website");
+
+// Answer of Question 2
+alert("Please enter your password");
+
+// Answer of Question 3
+alert("welcome to my js file!");
+
+// Answer of Question 4
+alert("Welcome to JS file.");
+alert("hello");
+
+// Answer of Question 5
+console.log("my js code ")
+
---
+++ script.js
@@ -0,0 +1,45 @@
+
+// Javascript Chapter 2 Test
+
+// Answer of Question 1 
+var username
+
+// Answer of Question 2
+var myName = "Uqba khan";
+
+// Answer of Question 3
+ var message 
+ var message = "Hello World";
+ alert(message);
+
+ // Answer of Question 4
+ alert("Student Bio")
+ var studentName = "Uqba Khan";
+ alert(studentName);
+ var studentAge = "16 yeras old";
+ alert(studentAge);
+ var skill = "fronted Developer";
+ alert(skill);
+
+ // Answer of Question 5
+ var computer = "computer"
+ alert(computer);
+
+ // Answer of Question 6
+ var text = "my email address is"
+ var email = "uqbakhan2006@gmail.com";
+ var gmail = text.concat("", email);
+ alert(gmail);
+
+ // Answer of Question 7
+ var book = "A smarter way to learn JavaScript"
+ alert(book);
+
+ // Answer of Question 8
+ document.write("I can write javascript ch 2");
+
+ // Answer of Question 9
+ console.write("hi");
+
+ // Answer of Question 10
+ alert("_______")
